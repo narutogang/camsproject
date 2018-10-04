3f2bbf5ed22f56a546606e9117e158e13dbfab3d
kaylinkhanal <kyalin.khanal@gmail.com>
2018-10-04 18:52:22 +0545

    just random commits

--- cams/camsapp/views.py
+++ cams/camsapp/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
-    return render(request, "index.html",{})
+    my_dict={'insert_me':"Hello i am from views.py "}
+    return render(request, "index.html",context=my_dict)
 
 def home(request):
     return render(request, "home.html",{})
 
 def about(request):
-    from cmspages.namer import bob
-    return render(request, "about.html",{"my_stuff":bob})
+
+    return render(request, "about.html",{})
 
 def contact(request):
     return render(request, "contact.html",{})
 
 def login(request):
-    return render(request, "login.html",{})
+    return render(request, "users/login.html",{})
